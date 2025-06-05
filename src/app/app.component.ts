161b21f334c3536a2fa4787a97c4680f6368e819
André Nicoletti <andregnicoletti@gmail.com>
2025-06-05 15:36:23 -0300

    event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,42 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'project-session-5';
+
+  inputText = 'Meu texto inicial';
+  inputType = 'text';
+  isDisabled = false;
+
+  enableInput() {
+    this.isDisabled = false;
+  }
+
+  disableInput() {
+    this.isDisabled = true;
+  }
+
+  setTextTypeInput() {
+    this.inputType = 'text';
+  }
+
+  setPasswordTypeInput() {
+    this.inputType = 'password';
+  }
+
+  logInputText() {
+    console.log(this.inputText);
+  }
+
+  handleInputKeyUp(event: KeyboardEvent) {
+    const currentText = (event.target as HTMLInputElement).value;
+    console.log(currentText);
+  }
+
+  handleInputEvent(event: Event){
+    const currentText = (event.target as HTMLInputElement).value
+    console.log("-> " + currentText)
+  }
 }
